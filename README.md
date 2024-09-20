892f34c5bf40bae5e77ffe6e02f4ec7eb6af179d
OscarPastural <oscar.pastural@edu.devinci.fr>
2024-09-20 11:16:40 +0200

    Small changes

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ Ce qu'il y a en dessous c'est juste le .tex en markdown
 
 Pour tester l'import, lancez juste data_import.py ça génèrera un fichier dans /data/
 
+# A FAIRE POUR LE PROJET 
+Les objectifs du projet sont les suivants :
+	Faire une analyse descriptive de la base de données pour bien la maîtriser et identifier les tâches de preprocessing pertinentes à réaliser. Toute action de preprocessing devra être explicitée, décrite et justifiée. Réfléchir aux unités de mesures des variables, à la nécessité de normaliser ou non les données, appliquer une ACP pour développer de l’intuition sur la problématique, etc. 
+	Identifier quelles sont les variables représentatives de la qualité de soudure et bien les comprendre. Réfléchir à la façon de prédire la qualité de soudure à partir de ces variables identifiées. Cela implique de réfléchir à une stratégie et il est attendu de bien l’expliciter et la justifier.
+	Appliquer différentes approches de ML vues en cours et en TD sur ce dataset pour prédire la qualité de soudure. Pensez à réaliser un protocole de validation croisée rigoureux. Enfin, le dataset n’étant pas complétement labélisé, nous vous invitons à effectuer un travail bibliographique sur des méthodes de ML avancées de type semi-supervisées. Vous êtes invités à en appliquer au minimum une en justifiant votre choix et la pertinence de celle-ci. 
+	Faire une analyse comparative des performances des différents modèles et méthodes appliqués. Il est donc nécessaire de réfléchir aux choix des métriques pertinentes. 
+	Conclure sur l’approche la plus appropriée pour prédire la qualité de soudure. 
+	Après cette étude : quelles sont vos recommandations pour obtenir une bonne qualité de soudure ?
+	Écrire un rapport d’environ 5 pages synthétisant le travail effectué et les résultats obtenus. Pensez à indiquer vos noms, le numéro de votre équipe, la date, vos mails et le titre sur la première page. Pensez aussi à mettre quelques figures illustratives. Il sera attendu un plan, une section rappelant la problématique du projet et votre traduction de celle-ci en problème/tâche de ML. Une section décrivant la base de données est aussi attendue (résultat d’ACP etc.), votre compréhension des variables cibles pour prédire la qualité de soudure et comment est-ce que vous proposez de vous y prendre. Une section sur les méthodes ML que vous choisissez d’appliquer avec vos justifications (pensez à les présenter synthétiquement, surtout si elles n’ont pas été vues en cours). Une section sur les résultats numériques sera à réaliser avec une étude comparative des performances. Les formules et/ou description des métriques utilisées devront être présentées. Enfin, pensez à ajouter une dernière section de discussion et conclusion avec un diagramme de GANNT contenant le partage en tâches, le temps que chaque tâche a pris et quel membre de l’équipe s’en est chargée. Bien entendu, une section sur les références bibliographiques clôturera le rapport. Pensez à bien citer vos références dans le corps de rapport. 
+	Remarques générales : un rapport doit être paginé, les figures et tableaux doivent avoir un titre et être cités dans le corps du rapport quand on y fait référence. 
+
+
 # MAP DATA LIBRARY
 
 ## Database MAP_DATA_WELD
--- data_import.py
+++ data_import.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# DATA IMPORT
-
+# Basic imports
+import re
 import pandas as pd
 import numpy as np
-import re
 
-from variables import FILE_MODIFIED_NAME, FILE_NAME
+# Import paths from variables.py
+from paths import DATA_PATH, FILE_MODIFIED_NAME, FILE_NAME
 
-
-# WE NEED TO MODIFY THE FILE TO REMOVE TRAILING WHITESPACES
-# WE ALSO NEED TO REMOVE DOUBLE SPACES
-# TO DO THAT, WE WILL DEFINE A FUNCTION TO PREPROCESS THE DATA
-
+# CONSTANTS
+CLEANED_CSV_PATH = DATA_PATH + "welddb_cleaned.csv"
 
 @staticmethod
 def preprocess_text_file(input_file: str, output_file: str):
     """
+    # WE NEED TO MODIFY THE FILE TO REMOVE TRAILING WHITESPACES
+    # WE ALSO NEED TO REMOVE DOUBLE SPACES
+    # TO DO THAT, WE WILL DEFINE A FUNCTION TO PREPROCESS THE DATA
+
     Removes trailing whitespaces from a file
     """
     with open(input_file, 'r', encoding='utf-8') as file:
@@ -29,59 +30,72 @@ def preprocess_text_file(input_file: str, output_file: str):
         file.writelines(processed_lines)
 
 
-preprocess_text_file(FILE_NAME, FILE_MODIFIED_NAME)
+def main():
+    """
+    Produces the data Import
+    - Removes unnecesary spaces
+    - Removes trailing spaces
+    - Changes "N" to NaN values
+    """
 
-# IMPORT THE FILE WITH PANDAS
-BASIC_DATAFRAME = pd.read_csv(FILE_MODIFIED_NAME, names=[
-    'Carbon concentration / weight %',
-    'Silicon concentration / weight %',
-    'Manganese concentration / weight %',
-    'Sulphur concentration / weight %',
-    'Phosphorus concentration / weight %',
-    'Nickel concentration / weight %',
-    'Chromium concentration / weight %',
-    'Molybdenum concentration / weight %',
-    'Vanadium concentration / weight %',
-    'Copper concentration / weight %',
-    'Cobalt concentration / weight %',
-    'Tungsten concentration / weight %',
-    'Oxygen concentration / parts per million by weight',
-    'Titanium concentration / parts per million by weight',
-    'Nitrogen concentration / parts per million by weight',
-    'Aluminium concentration / parts per million by weight',
-    'Boron concentration / parts per million by weight',
-    'Niobium concentration / parts per million by weight',
-    'Tin concentration / parts per million by weight',
-    'Arsenic concentration / parts per million by weight',
-    'Antimony concentration / parts per million by weight',
-    'Current / A',
-    'Voltage / V',
-    'AC or DC',
-    'Electrode positive or negative',
-    'Heat input / kJ mm^{-1}',
-    'Interpass temperature / °C',
-    'Type of weld',
-    'Post weld heat treatment temperature / °C',
-    'Post weld heat treatment time / hours',
-    'Yield strength / MPa',
-    'Ultimate tensile strength / MPa',
-    'Elongation / %',
-    'Reduction of Area / %',
-    'Charpy temperature / °C',
-    'Charpy impact toughness / J',
-    'Hardness / kg mm^{-2}',
-    '50% FATT',
-    'Primary ferrite in microstructure / %',
-    'Ferrite with second phase / %',
-    'Acicular ferrite / %',
-    'Martensite / %',
-    'Ferrite with carbide aggregate / %',
-    'Weld ID'
-],
-    delimiter=' ',
-    na_values=["N"]
-)
+    preprocess_text_file(FILE_NAME, FILE_MODIFIED_NAME)
 
-BASIC_DATAFRAME.replace("N", np.nan, inplace=True)
+    # IMPORT THE FILE WITH PANDAS
+    basic_dataframe = pd.read_csv(FILE_MODIFIED_NAME, names=[
+        'Carbon concentration / weight %',
+        'Silicon concentration / weight %',
+        'Manganese concentration / weight %',
+        'Sulphur concentration / weight %',
+        'Phosphorus concentration / weight %',
+        'Nickel concentration / weight %',
+        'Chromium concentration / weight %',
+        'Molybdenum concentration / weight %',
+        'Vanadium concentration / weight %',
+        'Copper concentration / weight %',
+        'Cobalt concentration / weight %',
+        'Tungsten concentration / weight %',
+        'Oxygen concentration / parts per million by weight',
+        'Titanium concentration / parts per million by weight',
+        'Nitrogen concentration / parts per million by weight',
+        'Aluminium concentration / parts per million by weight',
+        'Boron concentration / parts per million by weight',
+        'Niobium concentration / parts per million by weight',
+        'Tin concentration / parts per million by weight',
+        'Arsenic concentration / parts per million by weight',
+        'Antimony concentration / parts per million by weight',
+        'Current / A',
+        'Voltage / V',
+        'AC or DC',
+        'Electrode positive or negative',
+        'Heat input / kJ mm^{-1}',
+        'Interpass temperature / °C',
+        'Type of weld',
+        'Post weld heat treatment temperature / °C',
+        'Post weld heat treatment time / hours',
+        'Yield strength / MPa',
+        'Ultimate tensile strength / MPa',
+        'Elongation / %',
+        'Reduction of Area / %',
+        'Charpy temperature / °C',
+        'Charpy impact toughness / J',
+        'Hardness / kg mm^{-2}',
+        '50% FATT',
+        'Primary ferrite in microstructure / %',
+        'Ferrite with second phase / %',
+        'Acicular ferrite / %',
+        'Martensite / %',
+        'Ferrite with carbide aggregate / %',
+        'Weld ID'
+    ],
+        delimiter=' ',
+        na_values=["N"]
+    )
 
-BASIC_DATAFRAME.head(5)
+    basic_dataframe.replace("N", np.nan, inplace=True)
+
+    # Save the dataframe to a csv
+    basic_dataframe.to_csv(CLEANED_CSV_PATH)
+    basic_dataframe.head(5)
+
+if __name__ == "__main__":
+    main()
