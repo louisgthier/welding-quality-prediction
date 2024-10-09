00d37796f3ff68b2c1951589aca13b4ee63d7e25
Mcrash01 <90929317+Mcrash01@users.noreply.github.com>
2024-10-09 10:45:24 +0200

    ENH - add gloabl python variables in config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Global variables for the project
+
+quality_group_columns = ['Yield strength / MPa', 
+                 'Ultimate tensile strength / MPa', 
+                 'Elongation / %', 
+                 'Reduction of Area / %']
+charpy_group_columns = ['Charpy impact toughness / J',
+                 'Charpy temperature']
+target_columns=quality_group_columns+charpy_group_columns
